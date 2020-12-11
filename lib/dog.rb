3442299d211b364d23d782eb12fe739d02ca0bd7
woodelin-florveus <woodelinflorveus@gmail.com>
2020-12-10 23:08:02 -0500

    commit tree ec14e416194c839f5c3c0e46143bbaace8e22387

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,32 @@
-# Add your code here
+require "pry"
+
+class Dog
+
+    attr_reader :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        @@all.clear
+    end
+
+    def self.print_all
+       puts @@all.map {|one| one.name}
+    end
+
+    def save
+        @@all
+    end
+
+
+
+end
